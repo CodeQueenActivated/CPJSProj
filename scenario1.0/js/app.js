fb96ac746f8781e4896c5985b8781559cfd72f10
Darby J <codequeenactivated@gmail.com>
2024-05-28 04:11:48 -0400

    fix invalid email alert

--- scenario1.0/js/app.js
+++ scenario1.0/js/app.js
@@ -1,11 +1,39 @@
 window.onload = function() {
-    let emailState=false;
+    let emailState = false;
+
     let emailModal = document.
     getElementsByClassName('email-modal')[0];
 
     let closeModal = document.
-    getElementsByClassName('email-modal__close-btn')[0]
+    getElementsByClassName('email-modal__close-btn')[0];
 
+    let emailInput = document.
+    getElementsByClassName('email-modal__input')[0];
+
+    let emailButton = document.
+    getElementsByClassName('email-modal__button')[0];
+
+    function emailIsValid (email) {
+        return /\S+@\S+\.\S+/.test(email)
+      }
+    
+    emailButton.addEventListener('click', () => {
+        if(emailIsValid(emailInput.value)) {
+            console.log(emailInput.value)
+        }
+        else {
+        alert('this is not a valid email')
+        }
+    })
+
+    let showModal = () => {
+        if(emailState == false) {
+            emailModal.classList.add('email-modal--visible');
+            emailState = true
+            
+        }
+    }
+    
     closeModal.addEventListener('click', () => {
         emailModal.classList.remove('email-modal--visible')
 
@@ -15,12 +43,9 @@ window.onload = function() {
     console.log(emailModal)
     
     document.body.addEventListener('mouseleave', () => {
-        if(emailState == false) {
-        emailModal.classList.add('email-modal--visible')
-        console.log('mouse left')
-        }
+        showModal();
     });
 
    
-    console.log(document)
+   
 }
